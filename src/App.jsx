c0cb6e148a81c5dbc39c1c27fac39948e7f18af9
Santiago Freille <santiagofreille@gmail.com>
2024-08-28 20:58:23 -0300

    Mmodificacion del app para poder levatar. Se quitaron referencias. A solucionar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/menu_header';
 import Footer from './Components/footer';
 import Team from './Components/team';
 import Login from './Components/login';
-import Projects from './Components/projects';
-import Store from './Components/store';
-import Customs from './Components/customs';
+
 
 function App() {
   return (
@@ -18,9 +15,8 @@ function App() {
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<Team />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/store" element={<Store />} />
-            <Route path="/customs" element={<Customs />} />
+
+
             {/* Puedes agregar otras rutas aquí */}
           </Routes>
         </main>
